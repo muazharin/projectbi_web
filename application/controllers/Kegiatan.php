cefc8f44279024dfce0efdb61fb44355e5000830
root <alfanmuazharin@gmail.com>
2019-05-23 00:58:33 +0800

    menambahkan class baru

--- application/controllers/Kegiatan.php
+++ application/controllers/Kegiatan.php
@@ -15,4 +15,18 @@
         public function addDataEvent(){
             $this->M_kegiatan->addDataUserEvent();
         }
+
+        public function getData(){
+            $id_user = $this->uri->segment(3);
+            $mod = $this->M_kegiatan->getAllData($id_user);
+            $this->output->set_output(json_encode($mod, JSON_PRETTY_PRINT))->_display();
+            exit;
+        }
+
+        public function getPesertaKegiatan(){
+            $id_kegiatan = $this->uri->segment(3);
+            $mod2 = $this->M_kegiatan->getAllPesertaKegiatan($id_kegiatan);
+            $this->output->set_output(json_encode($mod2, JSON_PRETTY_PRINT))->_display();
+            exit;
+        }
     }
